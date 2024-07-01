9bb8b0f70c4594a80c3b1a325725d9557a80ac8e
Artyom Astafurov <artyom.astafurov@gmail.com>
2024-07-01 18:22:45 -0400

    add url to pdf example; refactor cli

---
+++ examples/audio_to_paper.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+URL=$1
+plato \
+    --prompt "Thoroughly review the <context> and write \"Introduction\" chapter for the paper. Make sure to include <markers>. Output as Markdown. Start with \"## Introduction\"" \
+    --context-size medium \
+    --inline-references \
+    --assemblyai-api-key $ASSEMBLYAI_API_KEY \
+    $URL > introduction.md
+plato \
+    --prompt "Thoroughly review the <context> and write \"Conclusion\" chapter for the paper. Make sure to include <markers>. Output as Markdown. Start with \"## Conclusion\"" \
+    --context-size medium \
+    --inline-references \
+    $URL > conclusion.md
+plato \
+    --prompt "Thoroughly review the <context> and identify the list of authors. Output as Markdown list: First Name, Last Name, Title, Organization. Start with \"## Authors\"" \
+    --context-size medium \
+    --inline-references \
+    $URL > authors.md
+plato \
+    --title \
+    $URL > title.md
+plato \
+    --abstract \
+    $URL > abstract.md
+plato \
+    --discussion \
+    --inline-references \
+    $URL > discussion.md
+plato \
+    --references \
+    $URL > references.md
+
+pandoc \
+    title.md \
+    authors.md \
+    abstract.md \
+    introduction.md \
+    discussion.md \
+    conclusion.md \
+    references.md \
+    -o output.pdf --from markdown
