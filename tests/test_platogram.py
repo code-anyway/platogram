678d51ac2acb026d6accde3ab264f4fcbd47345e
Artyom Astafurov <artyom.astafurov@gmail.com>
2024-08-21 16:47:33 -0400

    fix tests

--- tests/test_platogram.py
+++ tests/test_platogram.py
@@ -1,13 +1,15 @@
 import tempfile
+from datetime import datetime
 from pathlib import Path
 
 import pytest
-
+import web.main as web_main
 from web.main import (
     ConversionRequest,
     audio_to_paper,
     convert_and_send_with_error_handling,
     send_email,
+    Task,
 )
 
 
@@ -16,20 +18,11 @@ async def test_audio_to_paper():
     url = "https://www.youtube.com/shorts/rjVY7HXN6qA"
     lang = "en"
     with tempfile.TemporaryDirectory() as tmpdir:
-        stdout, stderr = await audio_to_paper(url, lang, Path(tmpdir))
+        stdout, stderr = await audio_to_paper(url, lang, Path(tmpdir), "artyom@codeanyway.com")
 
-        # Check if the expected files are present in the temporary directory
         output_files = list(Path(tmpdir).glob('*'))
-        
-        # Count the number of files for each extension
-        md_files = [f for f in output_files if f.suffix == '.md']
         pdf_files = [f for f in output_files if f.suffix == '.pdf']
-        docx_files = [f for f in output_files if f.suffix == '.docx']
-        
-        # Assert that there are exactly two files of each type
-        assert len(md_files) == 2, f"Expected 2 .md files, but found {len(md_files)}"
-        assert len(pdf_files) == 2, f"Expected 2 .pdf files, but found {len(pdf_files)}"
-        assert len(docx_files) == 2, f"Expected 2 .docx files, but found {len(docx_files)}"
+        assert len(pdf_files) == 2, f"Expected 2 .pdf files, but found {len(pdf_files)}.\n\n{stdout}\n\n{stderr}"
 
 
 @pytest.mark.asyncio
@@ -45,4 +38,5 @@ async def test_send_email():
 async def test_convert_and_send_http():
     request = ConversionRequest(payload="https://www.youtube.com/shorts/nXIHYB0Gp70", lang="en")
     user_id = "artyom.astafurov@gmail.com"
+    web_main.tasks[user_id] = Task(start_time=datetime.now(), request=request, status="running")
     await convert_and_send_with_error_handling(request, user_id)
